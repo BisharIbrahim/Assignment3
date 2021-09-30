0e29d1196a6afb638ebb1f42c1fcadf7da921a55
Bishar Ibrahim <45415723+BisharIbrahim@users.noreply.github.com>
2021-09-30 20:54:24 +1000

    AnimationController

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -14,13 +14,24 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Will turn player to the upwards position
         if (Input.GetKeyDown(KeyCode.UpArrow)) 
             PacStudent.SetTrigger("RotateUp");
+
+        //Will turn player to the Downwards position
         if (Input.GetKeyDown(KeyCode.DownArrow)) 
             PacStudent.SetTrigger("RotateDown");
+
+        //Will turn player to the Right position
         if (Input.GetKeyDown(KeyCode.RightArrow)) 
             PacStudent.SetTrigger("RotateRight");
+
+        //Will turn player to the Left position
         if (Input.GetKeyDown(KeyCode.LeftArrow)) 
             PacStudent.SetTrigger("RotateLeft");
+
+        //Simulate Death animation by pressing F
+        if (Input.GetKeyDown(KeyCode.F)) 
+            PacStudent.SetBool("isDead", true);
     }
 }
